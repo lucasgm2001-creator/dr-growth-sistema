b686f28e3c02bfe23f9cc49b57e63deb21a9d4cd
lucasgm2001-creator <lucasgm2001@gmail.com>
2026-05-17 14:14:26 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'drg-v3';
+const CACHE = 'drg-v4';
 
 self.addEventListener('install', () => self.skipWaiting());
 
@@ -7,8 +7,8 @@ self.addEventListener('activate', e => {
     caches.keys()
       .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
       .then(() => self.clients.claim())
-      .then(() => self.clients.matchAll({ type: 'window', includeUncontrolled: true }))
-      .then(clients => clients.forEach(c => c.navigate(c.url)))
+      .then(() => self.clients.matchAll({ type: 'window' }))
+      .then(clients => clients.forEach(c => c.postMessage({ type: 'RELOAD' })))
   );
 });
 
